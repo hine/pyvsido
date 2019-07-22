e8cb5196bd139487be63e5f8411d6b1d971c0f97
Daisuke IMAI <hine.gdw@gmail.com>
2019-07-22 16:28:48 +0900

    バージョン更新

--- vsido/__init__.py
+++ vsido/__init__.py
@@ -9,7 +9,7 @@ http://opensource.org/licenses/mit-license.php
 '''
 
 __author__ = 'Daisuke IMAI <hine.gdw@gmail.com>'
-__version__ = '0.1.2'
-__date__ = '24 Oct. 2015'
+__version__ = '0.1.3'
+__date__ = '22 Jul. 2019'
 
 from vsido.connect import Connect
