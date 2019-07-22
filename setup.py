4dd4b4e41e22ca941d981da28425e3d68404d92e
Daisuke IMAI <hine.gdw@gmail.com>
2019-07-22 17:00:02 +0900

    バージョン更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'pyvsido',
     packages = ['vsido'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'V-Sido CONNECT Livrary for Python3',
     author = 'Daisuke IMAI',
     author_email = 'hine.gdw@gmail.com',
@@ -10,7 +10,7 @@ setup(
     download_url = 'https://github.com/hine/pyvsido/archive/master.zip',
     keywords = ['robot', 'robotics', 'V-Sido', 'serial'],
     install_requires=[
-        'pyserial==2.6',
+        'pyserial',
     ],
     classifiers = [
         'Programming Language :: Python',
